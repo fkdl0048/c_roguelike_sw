fcbc5343642be69e0cc82a504856e1bb5a3b66d3
fkdl000048@naver.com <jeonglee>
2021-10-09 11:47:29 +0900

    s

---
+++ include/rogue.h
@@ -0,0 +1,26 @@
+#ifndef ROGUE_H
+#define ROGUE_H
+
+#include <ncurses.h>
+#include <stdlib.h>
+
+typedef struct
+{
+  int y;
+  int x;
+} Position;
+
+typedef struct
+{
+  Position pos;
+  char ch;
+} Entity;
+
+// player.c functions
+Entity* createPlayer(Position start_pos);
+void handleInput(int input);
+
+// externs
+extern Entity* player;
+
+#endif
