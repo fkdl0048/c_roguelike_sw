53694073932e5a0a1bb13c4619d12f4c36601fe3
이정안 <84510455+fkdl0048@users.noreply.github.com>
2021-10-23 21:28:33 +0900

    리펙토링

--- include/rogue.h
+++ include/rogue.h
@@ -1,26 +1,26 @@
 #ifndef ROGUE_H
 #define ROGUE_H
 
-#include <ncurses.h>
-#include <stdlib.h>
+#include <ncurses.h> // window.h랑 비슷한 헤더파일
+#include <stdlib.h> // 동적할당을 위해
 
-typedef struct
+typedef struct //구조체 선언
 {
   int y;
   int x;
 } Position;
 
-typedef struct
+typedef struct 
 {
   Position pos;
   char ch;
 } Entity;
 
 // player.c functions
-Entity* createPlayer(Position start_pos);
+Entity* createPlayer(Position start_pos); 
 void handleInput(int input);
 
 // externs
-extern Entity* player;
+extern Entity* player; //전역변수 공유 -> 다른 소스파일에 정의가 되어 있으니 찾아가서 사용할 것 이라는 뜻
 
 #endif
--- src/player.c
+++ src/player.c
@@ -1,35 +1,34 @@
 #include <rogue.h>
 
-
 Entity* createPlayer(Position start_pos)
 {
-  Entity* newPlayer = calloc(1, sizeof(Entity));
+  Entity* newPlayer = calloc(1, sizeof(Entity)); //동적할당 부분
 
-  newPlayer->pos.y = start_pos.y;
+  newPlayer->pos.y = start_pos.y; //위치, 캐릭터 모양 초기화 부분
   newPlayer->pos.x = start_pos.x;
   newPlayer->ch = '@';
 
-  return newPlayer;
+  return newPlayer; //해당 포인터 반환
 }
 
-void handleInput(int input)
+void handleInput(int input) 
 {
   switch(input)
   {
     //move up
-    case 'k':
-      player->pos.y--;
+    case 'w':
+      player->pos.y--; // player변수 공유 rogue.h헤더파일
       break;
     //move down
-    case 'j':
+    case 's':
       player->pos.y++;
       break;
     //move left
-    case 'h':
+    case 'a':
       player->pos.x--;
       break;
     //move right
-    case 'l':
+    case 'd':
       player->pos.x++;
       break;
     default:
