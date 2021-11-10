948189ec707083ade7c081b8723e5ccb5c25ed4e
lee jeongan <fkdl000048@gmail.com>
2021-11-10 17:48:04 +0900

    1110 version 0.7 frame update

--- src/draw.c
+++ src/draw.c
@@ -16,9 +16,31 @@ void drawEntity(Entity* entity)
     mvaddch(entity->pos.y, entity->pos.x, entity->ch);// y,x,표시할 문자 세가지를 인수로 받아 표시 *y먼저x가 뒤에온다*
 }
 
+void drawFrame(int x,int y,int width, int height)
+{
+    int i,j;
+    for(i = 1; i <= height;i++){
+        for (j = 1; j <= width; j++){
+            if((i == 1 || i == height) && (j == 1 || j == width)){
+                mvaddch(y+i,x+j,'*');
+            }
+            else if(i == 1 || i == height){
+                mvaddch(y+i,x+j,'-');
+            }
+            else if(j == 1 || j == width){
+                mvaddch(y+i,x+j,'|');
+            }
+        }
+    }
+}
+
 void drawEverything(void)
 {
     clear();
     drawMap();
+    drawFrame(94,0,27,3); // 현재위치
+    drawFrame(94,5,27,3); // 타이머
+    drawFrame(94,10,27,8); // 타이머
+    drawFrame(0,26,90,3); // 메세지 박스
     drawEntity(player);
 }
