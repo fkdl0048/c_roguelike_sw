658c590eb46eb373d34c923f87c307817ffc6f5c
이정안 <84510455+fkdl0048@users.noreply.github.com>
2021-11-11 21:51:31 +0900

    1111 version 2.4 -add Interaction motions add

--- src/lnteraction.c
+++ src/lnteraction.c
@@ -11,8 +11,19 @@ void downFloor(void){
 }
 
 void callInteraction(void){
-    //upFloor();
+    Position degPos[8] = {{-1, -1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
     Tile temp;
-    
+    Position pos = player->pos;
+    char interch;
+
+    for (int i = 0; i < 8; i++){
+        interch = map[pos.y+degPos[i].y][pos.x+degPos[i].x].ch;
+        if((interch == ']') || (interch == '[')){
+            if(curLocationFlag == 0)
+                upFloor();
+            else if(curLocationFlag == 1)
+                downFloor();
+        }
+    }
 }
 
